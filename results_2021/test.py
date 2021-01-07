ee38a9f4742208c6e0c78e5292d6203a813e0a6d
Vadim Kulyba <kulyba.vadim@gmail.com>
2021-01-07 17:08:30 +0300

    feat(lint): fix lint v.3

--- results_2021/test.py
+++ results_2021/test.py
@@ -1,7 +1,10 @@
 """Only for test lint."""
 
+
 def mul(first, second):
     """Test multiple function."""
     return first * second
 
-print(mul(3, 4))
+
+if __name__ == "__main__":
+    print(mul(3, 4))
