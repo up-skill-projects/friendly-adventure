2bb230cf4c0cd6d0badcea5a70cbab0efe8d6940
antonkostomaro <76551384+antonkostomaro@users.noreply.github.com>
2021-01-14 12:35:04 +0300

    Update c.py

--- results_2021/c.py
+++ results_2021/c.py
@@ -1,11 +1,28 @@
-def Pr(func):
-    print("decorator")
-    return Pr
-
-@Pr
-def Ss(x, y):
-    return x+y
-
-x=2
-y=2
-
+def calculate():
+     def nextInt():
+         return int(input())
+     def getOperator():
+         if token == "+":
+             return lambda x, y: x + y
+         if token == "-":
+             return lambda x, y: x - y
+         if token == "*":
+             return lambda x, y: x * y
+         if token == "/":
+             return lambda x, y: x / y
+         raise ValueError(f"Wrong operator: {token}")
+     stack = []
+     stack.append(nextInt())
+     stack.append(nextInt())
+     while stack[1:]:
+         token = input()
+         result = None
+         if token.isnumeric():
+             result = int(token)
+         elif token in ("+", "-", "*", "/",):
+             result = getOperator()(stack.pop(), stack.pop())
+         else:
+             raise ValueError(f"Wrong token: {token}")
+         stack.append(result)
+     return stack.pop()
+print(calculate())
