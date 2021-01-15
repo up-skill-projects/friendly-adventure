6f1758e2140d3f3e5464983a24f82eee68ec6559
antonkostomaro <76551384+antonkostomaro@users.noreply.github.com>
2021-01-15 20:34:31 +0300

    Update program.py

--- results_2021/program.py
+++ results_2021/program.py
@@ -1,8 +1,8 @@
-def calculate():
-    def nextInt():
+def Сalculate():
+    def next_Int():
         return int(input())
 
-    def getOperator():
+    def get_Operator():
         if token == "+":
             return lambda x, y: x + y
         if token == "-":
@@ -14,19 +14,19 @@ def calculate():
         raise ValueError(f"Wrong operator: {token}")
 
     stack = []
-    stack.append(nextInt())
-    stack.append(nextInt())
+    stack.append(next_Int())
+    stack.append(next_Int())
     while stack[1:]:
         token = input()
         result = None
         if token.isnumeric():
             result = int(token)
         elif token in ("+", "-", "*", "/",):
-            result = getOperator()(stack.pop(), stack.pop())
+            result = get_Operator()(stack.pop(), stack.pop())
         else:
             raise ValueError(f"Wrong token: {token}")
         stack.append(result)
     return stack.pop()
 
 
-print(calculate())
+print(Сalculate())
