0a335f2b30a7035d6fe13f8486acde3e5fc7ac16
antonkostomaro <76551384+antonkostomaro@users.noreply.github.com>
2021-01-19 23:00:52 +0300

    Update program.py

--- results_2021/program.py
+++ results_2021/program.py
@@ -1,10 +1,8 @@
 def fun_calcul():
     """Return the pathname of the KOS root directory."""
     def next_int():
-        """Return the pathname of the KOS root directory."""
         return int(input())
     """Return the pathname of the KOS root directory."""
-
     def get_operator():
         """Return the pathname of the KOS root directory."""
         if token == "+":
@@ -16,13 +14,10 @@ def fun_calcul():
         if token == "/":
             return lambda x, y: x / y
         raise ValueError(f"Wrong operator: {token}")
-    """Return the pathname of the KOS root directory."""
     stack = []
     stack.append(next_int())
     stack.append(next_int())
-    """Return the pathname of the KOS root directory."""
     while stack[1:]:
-        """Return the pathname of the KOS root directory."""
         token = input()
         # result = None
         if token.isnumeric():
@@ -33,6 +28,6 @@ def fun_calcul():
             raise ValueError(f"Wrong token: {token}")
         stack.append(result)
     return stack.pop()
-    
-        
+
+
 print(fun_calcul())
