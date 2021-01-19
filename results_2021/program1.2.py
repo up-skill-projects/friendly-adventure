c836e9514d3a998eb5bfb6de5f9bb2357c312d3e
antonkostomaro <76551384+antonkostomaro@users.noreply.github.com>
2021-01-19 23:30:41 +0300

    Update program1.2.py

--- results_2021/program1.2.py
+++ results_2021/program1.2.py
@@ -2,7 +2,7 @@
 act = input("Какое действие будем выполнять? ")
 """ калькулятор обнуляющие биты """
 MY_STR = ''
-number_two = 2
+NUMBER_TWO = 2
 if act == '^':
     number = int(input("введите число => "))
     second = int(input("В какую степень будем возводить? "))
@@ -11,12 +11,12 @@ elif act == '!':
     number = int(input("Введите число=> "))
     bits = int(input('число бит: '))
     while number > 0:
-        if bits and number % number_two:
+        if bits and number % NUMBER_TWO:
             MY_STR = '0' + MY_STR
             bits -= 1
         else:
-            MY_STR = str(number % number_two) + MY_STR
-        number = number // number_two
-    print("Результат=>", int(MY_STR, number_two))
+            MY_STR = str(number % NUMBER_TWO) + MY_STR
+        number = number // NUMBER_TWO
+    print("Результат=>", int(MY_STR, NUMBER_TWO))
 else:
     print('Не верное действие!')
