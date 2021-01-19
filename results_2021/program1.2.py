94f4e74d94ea31c1020186ba93c98c8d0b064f33
antonkostomaro <76551384+antonkostomaro@users.noreply.github.com>
2021-01-19 23:15:04 +0300

    Update program1.2.py

--- results_2021/program1.2.py
+++ results_2021/program1.2.py
@@ -1,19 +1,19 @@
-x = input("Какое действие будем выполнять? ")
-b = ''
-if x == '^':
-    n = int(input("введите число => "))
-    y = int(input("В какую степень будем возводить? "))
-    print("Результат =>", n**y)
-elif x == '!':
-    n = int(input("Введите число=> "))
-    m = int(input('число бит: '))
-    while n > 0:
-        if m and n % 2:
-            b = '0' + b
-            m -= 1
+act = input("Какое действие будем выполнять? ")
+strings = ''
+if act == '^':
+    number = int(input("введите число => "))
+    second = int(input("В какую степень будем возводить? "))
+    print("Результат =>", number**second)
+elif act == '!':
+    number = int(input("Введите число=> "))
+    bits = int(input('число бит: '))
+    while number > 0:
+        if bits and number % 2:
+            strings = '0' + strings
+            bits -= 1
         else:
-            b = str(n % 2) + b
-        n = n // 2
-    print("Результат=>", int(b, 2))
+            strings = str(number % 2) + strings
+        number = number // 2
+    print("Результат=>", int(strings, 2))
 else:
     print('Не верное действие!')
